8f90fdb2f10f17a07fc79e3f95cc62c3b1bd067e
Francis M. Tyers <ftyers@prompsit.com>
2011-04-02 13:20:48 +0000

    some fixes, now generates properly

--- recompile.sh
+++ recompile.sh
@@ -5,6 +5,6 @@ lt-comp rl apertium-mt-he.he.dix mt-he.autogen.bin
 lt-comp lr apertium-mt-he.he.dix he-mt.automorf.bin
 lt-comp rl apertium-mt-he.mt.dix he-mt.autogen.bin
 lt-comp lr apertium-mt-he.mt-he.dix mt-he.autobil.bin
-lt-comp rl apertium-mt-he.mt-he.dix he-mt.aotobil.bin
+lt-comp rl apertium-mt-he.mt-he.dix he-mt.autobil.bin
 apertium-preprocess-transfer apertium-mt-he.mt-he.t1x mt-he.t1x.bin
 
