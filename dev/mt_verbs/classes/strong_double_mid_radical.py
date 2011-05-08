392fba10a27e1d0410a4a7d4219d2ea76b3ed155
n0nick <n0nick@oqaa.projectjj.com>
2011-05-08 20:44:14 +0000

    fixing strong_double_mid_radical according to kisser example on http://www.phil-fak.uni-duesseldorf.de/summerschool2002/Hoberman.pdf

--- dev/mt_verbs/classes/strong_double_mid_radical.py
+++ dev/mt_verbs/classes/strong_double_mid_radical.py
@@ -3,71 +3,71 @@
 # -*- encoding: utf-8 -*-
 
 def past_p1_sg(stem, root, vowels):
-	# kiser; kisirt; past.p1.sg; vblex
+	# kisser; kissirt; past.p1.sg; vblex
 	return root[0] + vowels[0] + root[1] + root[2] + 'i' + root[3] + 't'
 	
 def past_p2_sg(stem, root, vowels):
-	# kiser; kisirt; past.p2.sg; vblex
+	# kisser; kissirt; past.p2.sg; vblex
 	return root[0] + vowels[0] + root[1] + root[2] + 'i' + root[3] + 't'
 	
 def past_p3_f_sg(stem, root, vowels):
-	# kiser; kisret; past.p3.f.sg; vblex
+	# kisser; kissret; past.p3.f.sg; vblex
 	return root[0] + vowels[0] + root[1] + root[2] + root[3] + 'et'
 	
 def past_p1_pl(stem, root, vowels):
-	# kiser; kisirna; past.p1.pl; vblex
+	# kisser; kissirna; past.p1.pl; vblex
 	return root[0] + vowels[0] + root[1] + root[2] + 'i' + root[3] + 'na'
 	
 def past_p2_pl(stem, root, vowels):
-	# kiser; ksirtu; past.p2.pl; vblex
-	return root[0] + root[1] + root[2] + 'i' + root[3] + 'tu'
+	# kisser; kissurtu; past.p2.pl; vblex
+	return root[0] + vowels[0] + root[1] + root[2] + 'u' + root[3] + 'tu'
 	
 def past_p3_pl(stem, root, vowels):
-	# kiser; kisru; past.p3.pl; vblex
+	# kisser; kissru; past.p3.pl; vblex
 	return root[0] + vowels[0] + root[1] + root[2] + root[3] + 'u'
 	
 def pres_p1_sg(stem, root, vowels):
-	# kiser; inkiser; pres.p1.sg; vblex
-	return 'in' + root[0] + vowels[0] + root[1] + root[2] + vowels[1] + root[3]
+	# kisser; nkisser; pres.p1.sg; vblex
+	return 'n' + root[0] + vowels[0] + root[1] + root[2] + vowels[1] + root[3]
 	
 def pres_p2_sg(stem, root, vowels):
-	# kiser; tkiser; pres.p2.sg; vblex
+	# kisser; tkisser; pres.p2.sg; vblex
 	return 't' + root[0] + vowels[0] + root[1] + root[2] + vowels[1] + root[3]
 	
 def pres_p3_m_sg(stem, root, vowels):
-	# kiser; ikiser; pres.p3.m.sg; vblex
-	return 'i' + root[0] + vowels[0] + root[1] + root[2] + vowels[1] + root[3]
+	# kisser; jkisser; pres.p3.m.sg; vblex
+	return 'j' + root[0] + vowels[0] + root[1] + root[2] + vowels[1] + root[3]
 	
 def pres_p3_f_sg(stem, root, vowels):
-	# kiser; tkiser; pres.p3.f.sg; vblex
+	# kisser; tkisser; pres.p3.f.sg; vblex
 	return 't' + root[0] + vowels[0] + root[1] + root[2] + vowels[1] + root[3]
 	
 def pres_p1_pl(stem, root, vowels):
-	# kiser; inkisru; pres.p1.pl; vblex
-	return 'in' + root[0] + vowels[0] + root[1] + root[2] + root[3] + 'u'
+	# kisser; nkissru; pres.p1.pl; vblex
+	return 'n' + root[0] + vowels[0] + root[1] + root[2] + root[3] + 'u'
 	
 def pres_p2_pl(stem, root, vowels):
-	# kiser; tkisru; pres.p2.pl; vblex
+	# kisser; tkissru; pres.p2.pl; vblex
 	return 't' + root[0] + vowels[0] + root[1] + root[2] + root[3] + 'u'
 	
 def pres_p3_pl(stem, root, vowels):
-	# kiser; ikisru; pres.p3.pl; vblex
-	return 'i' + root[0] + vowels[0] + root[1] + root[2] + root[3] + 'u'
+	# kisser; jkissru; pres.p3.pl; vblex
+	return 'j' + root[0] + vowels[0] + root[1] + root[2] + root[3] + 'u'
 	
 def imp_p2_pl(stem, root, vowels):
-	# kiser; kisru; imp.p2.pl; vblex
+	# kisser; kissru; imp.p2.pl; vblex
 	return root[0] + vowels[0] + root[1] + root[2] + root[3] + 'u'
 	
 def pp_m_sg(stem, root, vowels):
-	# kiser; imkiser; pp.m.sg; vblex
+	# kisser; imkisser; pp.m.sg; vblex
 	return 'im' + root[0] + vowels[0] + root[1] + root[2] + vowels[1] + root[3]
 	
 def pp_f_sg(stem, root, vowels):
-	# kiser; imkisra; pp.f.sg; vblex
+	# kisser; imkissra; pp.f.sg; vblex
 	return 'im' + root[0] + vowels[0] + root[1] + root[2] + root[3] + 'a'
 	
 def pp_pl(stem, root, vowels):
-	# kiser; imkisrin; pp.pl; vblex
+	# kisser; imkissrin; pp.pl; vblex
 	return 'im' + root[0] + vowels[0] + root[1] + root[2] + root[3] + 'in'
 
 def main(stem, root, vowels):
