95a9e3f9b8b225d07283a72e3b9b8e8ef1f67da3
n0nick <n0nick@oqaa.projectjj.com>
2011-05-21 17:09:54 +0000

    adding defective a_a subclass

--- dev/mt_verbs/classes/__init__.py
+++ dev/mt_verbs/classes/__init__.py
@@ -1,4 +1,4 @@
 import strong, strong_a_o, strong_a_o_overloaded, strong_double_mid_radical
-import defective
+import defective, defective_a_a
 import loan_romance, loan_english
 import irregular_ra, irregular_qal, irregular_gie, irregular_ta, irregular_ha
