36de04f15a4367de4e503c6584b1e1a99a80d47c
n0nick <n0nick@oqaa.projectjj.com>
2011-05-22 07:55:16 +0000

    adding quadriliteral verbs class

--- dev/mt_verbs/classes/__init__.py
+++ dev/mt_verbs/classes/__init__.py
@@ -1,4 +1,5 @@
 import strong, strong_a_o, strong_a_o_overloaded, strong_double_mid_radical
 import defective, defective_a_a
+import quadriliteral
 import loan_romance, loan_english
 import irregular_ra, irregular_qal, irregular_gie, irregular_ta, irregular_ha
