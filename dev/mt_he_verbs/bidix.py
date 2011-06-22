c1ab28a9d56531e7e3bd33f60ad394f4c63c95b1
n0nick <n0nick@oqaa.projectjj.com>
2011-06-22 12:16:07 +0000

    removing unnecessary function'

--- dev/mt_he_verbs/bidix.py
+++ dev/mt_he_verbs/bidix.py
@@ -9,16 +9,6 @@ sys.stdout = codecs.getwriter('utf-8')(sys.stdout)
 sys.stderr = codecs.getwriter('utf-8')(sys.stderr)
 reload(sys); sys.setdefaultencoding("utf-8")
 
-
-def format_entry(FORMAT, stem, form, pos, feat):
-	tags = ''.join(['<s n="%s"/>' % tag
-			for tag in [pos] + feat.split('.')])
-	tags = tags.replace('<s n="+neg"/>', '<j/>x<s n="neg"/>'); # TODO: what should the negative lemma be?
-	tags = tags.replace('<s n="+probj"/>', '<j/>probj<s n="prn"/><s n="pers"/>')
-	return "    <e><p><l>%s</l>\t<r>%s%s</r></p></e>" % (speling[feat],
-							     stem,
-							     tags);
-
 print '  <!-- SECTION: Verbs -->';
 print '    <!-- Generated on: ' + time.strftime('%Y-%m-%d %H:%M %Z') + ' -->'; 
 
