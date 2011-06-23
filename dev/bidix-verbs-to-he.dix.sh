d8b2038f5396b481f20c3e91edefe6bf1ed40ef8
Kevin Brubeck Unhammer <unhammer@fsfe.org>
2011-06-23 10:27:50 +0000

    exchange sed for awk since mac sed is buggy hell

--- dev/bidix-verbs-to-he.dix.sh
+++ dev/bidix-verbs-to-he.dix.sh
@@ -2,7 +2,7 @@
 echo "    <!-- SECTION: Verbs, converted from n0nick's modified hspell, see dev/hspell-to-dix.sh -->"
 GREPS=`mktemp -t greps.XXXXXXXX`;
 
-grep '<r>.*<s n="vblex"/>' apertium-mt-he.mt-he.dix |sed 's%.*<r>\(.*\)<s n="vblex"/>.*%<e lm="\1">%' > "$GREPS"
+awk -F'<r>|<s n="vblex"/></r>' '/<r>.*<s n="vblex"\/>/ {print "<e lm=\""$2"\">"}' apertium-mt-he.mt-he.dix > "$GREPS"
 grep -f "$GREPS" -F dev/he.verbs.dix |\
 # add line-break on new lemma:
 awk -F'"' '//{if($2 != lm) {lm=$2;print "";} print}'
