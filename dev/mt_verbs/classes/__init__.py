a43fad79a8e538a5b40725ed5d8f1f32b1dc5620
Francis M. Tyers <ftyers@prompsit.com>
2011-07-02 22:53:06 +0000

    more stuff

--- dev/mt_verbs/classes/__init__.py
+++ dev/mt_verbs/classes/__init__.py
@@ -1,6 +1,6 @@
 import strong, strong_a_o, strong_a_o_overloaded, strong_double_mid_radical, strong_double_mid_radical_initial_cluster
-import defective, defective_a_a
+import defective, defective_a_a, defective_p3_only
 import quadriliteral
 import loan_romance, loan_english
-import irregular_ra, irregular_qal, irregular_gie, irregular_ta, irregular_ha
+import irregular_ra, irregular_qal, irregular_gie, irregular_ta, irregular_ha, irregular_sar
 import vaux_kien
