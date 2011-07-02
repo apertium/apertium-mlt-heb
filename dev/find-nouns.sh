4a3f0a9ab5e36c315c809734f7c5f5037a5dac08
Francis M. Tyers <ftyers@prompsit.com>
2011-07-02 16:11:01 +0000

    add script to find a specific noun

--- dev/find-nouns.sh
+++ dev/find-nouns.sh
@@ -1,5 +1,5 @@
 CORPUS=/home/fran/corpora/maltese/scannell/mt.crp.txt
 
-cat $CORPUS | apertium -d ../ mt-he-morph | grep '<det><def><mf><sp>\$ \^[a-z]\+\/\*[a-z]\+\$ ^[a-z]\+\/[a-z]\+<adj><\(m\|f\)><sg>\$' | sed 's/<det><def><mf><sp>\$ \^[a-z]\+\/\*[a-z]\+\$ ^[a-z]\+\/[a-z]\+<adj><\(m\|f\)><sg>\$/@&@/g' | cut -f2 -d'@' > /tmp/mt-noun-candidates.txt
+cat $CORPUS | apertium -d ../ mt-he-morph | grep '<det><def><mf><sp>\$ \^[ABĊDEFĠGGHĦIIJKLMNOPQRSTUVWXZŻabċdefġgħghieiejklmnopqrstuvwxzżycYCáéíóúàèìòùñöëäïüç]\+\/\*[ABĊDEFĠGGHĦIIJKLMNOPQRSTUVWXZŻabċdefġgħghieiejklmnopqrstuvwxzżycYCáéíóúàèìòùñöëäïüç]\+\$ ^[ABĊDEFĠGGHĦIIJKLMNOPQRSTUVWXZŻabċdefġgħghieiejklmnopqrstuvwxzżycYCáéíóúàèìòùñöëäïüç]\+\/[ABĊDEFĠGGHĦIIJKLMNOPQRSTUVWXZŻabċdefġgħghieiejklmnopqrstuvwxzżycYCáéíóúàèìòùñöëäïüç]\+<adj><\(m\|f\)><sg>\$' | sed 's/<det><def><mf><sp>\$ \^[ABĊDEFĠGGHĦIIJKLMNOPQRSTUVWXZŻabċdefġgħghieiejklmnopqrstuvwxzżycYCáéíóúàèìòùñöëäïüç]\+\/\*[ABĊDEFĠGGHĦIIJKLMNOPQRSTUVWXZŻabċdefġgħghieiejklmnopqrstuvwxzżycYCáéíóúàèìòùñöëäïüç]\+\$ ^[ABĊDEFĠGGHĦIIJKLMNOPQRSTUVWXZŻabċdefġgħghieiejklmnopqrstuvwxzżycYCáéíóúàèìòùñöëäïüç]\+\/[ABĊDEFĠGGHĦIIJKLMNOPQRSTUVWXZŻabċdefġgħghieiejklmnopqrstuvwxzżycYCáéíóúàèìòùñöëäïüç]\+<adj><\(m\|f\)><sg>\$/@&@/g' | cut -f2 -d'@' > /tmp/mt-noun-candidates.txt
 
 
