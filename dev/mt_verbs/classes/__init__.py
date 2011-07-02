1df1f0235143ff7fadde4574a01da6cc67e890fc
Francis M. Tyers <ftyers@prompsit.com>
2011-07-02 18:17:40 +0000

    more stuff +1 verb, some interrogatives etc.

--- dev/mt_verbs/classes/__init__.py
+++ dev/mt_verbs/classes/__init__.py
@@ -1,4 +1,4 @@
-import strong, strong_a_o, strong_a_o_overloaded, strong_double_mid_radical
+import strong, strong_a_o, strong_a_o_overloaded, strong_double_mid_radical, strong_double_mid_radical_initial_cluster
 import defective, defective_a_a
 import quadriliteral
 import loan_romance, loan_english
