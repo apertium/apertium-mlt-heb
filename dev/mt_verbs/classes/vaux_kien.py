46889d821ba455f7daa3b565cd350671ba70893a
Francis M. Tyers <ftyers@prompsit.com>
2011-07-03 23:14:33 +0000

    j- initial alternatives adding to kien, thanks Adam! :)

--- dev/mt_verbs/classes/vaux_kien.py
+++ dev/mt_verbs/classes/vaux_kien.py
@@ -2,17 +2,28 @@
 # coding=utf-8
 # -*- encoding: utf-8 -*-
 
+
+#My guess is that the forms in -u are plural; as for the difference between
+#j-initial and i-initial, this has to do, I believe, with whether the
+#preceding word ends in a consonant or vowel: if it ends in a vowel, then
+#the j-initial form is used; if it ends in a consonant, then the i-initial
+#form is used. 
+
+
 def main(stem, root, vowels):
 	sp = {}
 
 	sp['inf'] = stem
 	sp['past.p1.sg'] = 'inkun'
+	sp['past.p1.sg'] = 'jnkun'
 	sp['past.p2.sg'] = 'tkun' # thun
 	sp['past.p3.m.sg'] = 'ikun'
+	sp['past.p3.m.sg'] = 'jkun'
 	sp['past.p3.f.sg'] = 'tkun' #thun
 	sp['past.p1.pl'] = 'nkunu'
 	sp['past.p2.pl'] = 'tkunu' # tirunu
 	sp['past.p3.pl'] = 'ikunu'
+	sp['past.p3.pl'] = 'jkunu' 
 	sp['pres.p1.sg'] = 'kont'
 	sp['pres.p2.sg'] = 'kont'
 	sp['pres.p3.m.sg'] = 'kien'
