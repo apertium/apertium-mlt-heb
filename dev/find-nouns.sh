e33b0b78ee97edc2d1ed3700aecd5d70f3100890
Francis M. Tyers <ftyers@prompsit.com>
2011-07-03 20:45:59 +0000

    adverb and preposition

--- dev/find-nouns.sh
+++ dev/find-nouns.sh
@@ -1,4 +1,5 @@
-CORPUS=/home/fran/corpora/maltese/scannell/mt.crp.txt
+#CORPUS=/home/fran/corpora/maltese/scannell/mt.crp.txt
+CORPUS=/home/fran/corpora/maltese/combined/mt.crp.txt
 
 cat $CORPUS | apertium -d ../ mt-he-morph | grep -i '<det><def><mf><sp>\$ \^[ABĊDEFĠGGHĦIIJKLMNOPQRSTUVWXZŻabċdefġgħghieiejklmnopqrstuvwxzżycYCáéíóúàèìòùñöëäïüç]\+\/\*[ABĊDEFĠGGHĦIIJKLMNOPQRSTUVWXZŻabċdefġgħghieiejklmnopqrstuvwxzżycYCáéíóúàèìòùñöëäïüç]\+\$ ^[ABĊDEFĠGGHĦIIJKLMNOPQRSTUVWXZŻabċdefġgħghieiejklmnopqrstuvwxzżycYCáéíóúàèìòùñöëäïüç]\+\/[ABĊDEFĠGGHĦIIJKLMNOPQRSTUVWXZŻabċdefġgħghieiejklmnopqrstuvwxzżycYCáéíóúàèìòùñöëäïüç]\+<adj><\(m\|f\)><sg>\$' | sed 's/<det><def><mf><sp>\$ \^[ABĊDEFĠGGHĦIIJKLMNOPQRSTUVWXZŻabċdefġgħghieiejklmnopqrstuvwxzżycYCáéíóúàèìòùñöëäïüç]\+\/\*[ABĊDEFĠGGHĦIIJKLMNOPQRSTUVWXZŻabċdefġgħghieiejklmnopqrstuvwxzżycYCáéíóúàèìòùñöëäïüç]\+\$ ^[ABĊDEFĠGGHĦIIJKLMNOPQRSTUVWXZŻabċdefġgħghieiejklmnopqrstuvwxzżycYCáéíóúàèìòùñöëäïüç]\+\/[ABĊDEFĠGGHĦIIJKLMNOPQRSTUVWXZŻabċdefġgħghieiejklmnopqrstuvwxzżycYCáéíóúàèìòùñöëäïüç]\+<adj><\(m\|f\)><sg>\$/@&@/g' | cut -f2 -d'@' > /tmp/mt-noun-candidates-sg.txt
 
