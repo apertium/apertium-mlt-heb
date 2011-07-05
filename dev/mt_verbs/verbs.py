d898bf39ca6c603ae74aabed73ea7fb878867905
n0nick <n0nick@oqaa.projectjj.com>
2011-07-05 18:34:44 +0000

    nicer error message, tabs->spaces

--- dev/mt_verbs/verbs.py
+++ dev/mt_verbs/verbs.py
@@ -46,55 +46,55 @@ if STEMSFILE:
         sys.exit(1)
 
 def format_entry(FORMAT, stem, form, pos, feat):
-	if FORMAT=="speling":
-		return stem + '; ' + speling[feat] + '; ' + feat + '; ' + pos;
-	elif FORMAT=="dix":
-		tags = ''.join(['<s n="%s"/>' % tag
-				for tag in [pos] + feat.split('.')])
-		tags = tags.replace('<s n="+neg"/>', '<j/>x<s n="neg"/>'); # TODO: what should the negative lemma be?
-		tags = tags.replace('<s n="+probj"/>', '<j/>prpers<s n="prn"/><s n="obj"/>')
-		return "    <e><p><l>%s</l>\t<r>%s%s</r></p></e>" % (speling[feat],
-								     stem,
-								     tags);
-	else:
-		raise(Exception);
+    if FORMAT=="speling":
+        return stem + '; ' + speling[feat] + '; ' + feat + '; ' + pos;
+    elif FORMAT=="dix":
+        tags = ''.join(['<s n="%s"/>' % tag
+                for tag in [pos] + feat.split('.')])
+        tags = tags.replace('<s n="+neg"/>', '<j/>x<s n="neg"/>'); # TODO: what should the negative lemma be?
+        tags = tags.replace('<s n="+probj"/>', '<j/>prpers<s n="prn"/><s n="obj"/>')
+        return "    <e><p><l>%s</l>\t<r>%s%s</r></p></e>" % (speling[feat],
+                                     stem,
+                                     tags);
+    else:
+        raise(Exception);
 
 if FORMAT=="dix": 
-	print '  <section id="verbs" type="standard">';
-	print '    <!-- Generated on: ' + time.strftime('%Y-%m-%d %H:%M %Z') + ' -->'; 
+    print '  <section id="verbs" type="standard">';
+    print '    <!-- Generated on: ' + time.strftime('%Y-%m-%d %H:%M %Z') + ' -->'; 
 
 
 for line in lines:
-	if len(line) < 2 or line[0] == '#':
-		continue
-	
-	row = line.split(',')
-	
-	#stem, category, gloss, root, vowels, subclass, checked
-	stem = row[0].strip()
-	category = row[1].strip()
-	root = row[3].strip().split('-')
-	vowels = row[4].strip().split('-')
-	subclass = row[5].strip() if (len(row) >= 5) else None
-	pos = 'vaux' if category == 'vaux' else 'vblex'
-	
-	# build class name
-	classname = category
-	if subclass:
-		classname = classname + '_' + subclass
+    if len(line) < 2 or line[0] == '#':
+        continue
+    
+    row = line.split(',')
+    
+    #stem, category, gloss, root, vowels, subclass, checked
+    stem = row[0].strip()
+    category = row[1].strip()
+    root = row[3].strip().split('-')
+    vowels = row[4].strip().split('-')
+    subclass = row[5].strip() if (len(row) >= 5) else None
+    pos = 'vaux' if category == 'vaux' else 'vblex'
+    
+    # build class name
+    classname = category
+    if subclass:
+        classname = classname + '_' + subclass
 
-	try:
-		klass = getattr(classes, classname)
-		speling = klass.main(stem, root, vowels)
-		feats = [f for f in speling.keys() if speling[f]]
-		feats.sort()	# pretty
-		for feat in feats:
-			print format_entry(FORMAT, stem, speling[feat], pos, feat);
-			
-	except AttributeError:
-		print 'MISSING CLASS:', classname
-		
-	print '' # newline between words
+    try:
+        klass = getattr(classes, classname)
+        speling = klass.main(stem, root, vowels)
+        feats = [f for f in speling.keys() if speling[f]]
+        feats.sort()    # pretty
+        for feat in feats:
+            print format_entry(FORMAT, stem, speling[feat], pos, feat);
+            
+    except AttributeError:
+        sys.stderr.write("Error: Missing class '{0}'".format(classname))
+        
+    print '' # newline between words
 
 
 if FORMAT=="dix": print '  </section>';
