42e7acf1cc5f037ed94c5b7ddd2894d0cfcb13e9
n0nick <n0nick@oqaa.projectjj.com>
2011-07-09 13:52:04 +0000

    fixing mark_analysed.py - complete

--- dev/freq_cat/mark_analysed.py
+++ dev/freq_cat/mark_analysed.py
@@ -2,31 +2,40 @@
 # coding=utf-8
 # -*- encoding: utf-8 -*-
 
-import sys, codecs, re;
+import sys, codecs
+from subprocess import Popen, PIPE
 
 sys.stdin  = codecs.getreader('utf-8')(sys.stdin)
 sys.stdout = codecs.getwriter('utf-8')(sys.stdout)
 sys.stderr = codecs.getwriter('utf-8')(sys.stderr)
 reload(sys); sys.setdefaultencoding("utf-8")
 
-if len(sys.argv)<2:
-    print "Usage: mark_analysed.py <freqlist>"
+if len(sys.argv)<3:
+    print "Usage: mark_analysed.py <freqlist> <automorf>"
     sys.exit(1)
 
+freqlist = sys.argv[1]
+automorf = sys.argv[2]
+
 try:
-    lines = file(sys.argv[1])
+    lines = file(freqlist)
 except IOError as e:
-    sys.stderr.write("Error reading file {0}\n".format(sys.argv[1]))
+    sys.stderr.write("Error reading file {0}\n".format(freqlist))
     sys.exit(1)
 
 # go over freqlist and check analyse
-p = re.compile('\^.*\/\*(.*)\$')
 for line in lines:
-    match = p.match(line.strip())
-    key = match.group(1).lower() if match else None
-    print key
-
-    if key:
-        print "+" + key
-    else:
+    try:
+        word = line.split('^')[2].split('/')[0]
+    except IndexError:
         sys.stdout.write(line)
+        continue
+
+    p1 = Popen(["echo", word], stdout=PIPE)
+    p2 = Popen(["lt-proc", automorf], stdin=p1.stdout, stdout=PIPE)
+    p1.stdout.close()
+    out = p2.communicate()[0]
+
+    if out.find('*') == -1:
+        sys.stdout.write(';') 
+    sys.stdout.write(line)
