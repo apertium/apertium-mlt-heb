50ca2edcb96d7883d8df60b4699b75c615c5971a
n0nick <n0nick@oqaa.projectjj.com>
2011-07-13 14:22:10 +0000

    adding some irregular verbs

--- dev/mt_verbs/classes/__init__.py
+++ dev/mt_verbs/classes/__init__.py
@@ -1,7 +1,17 @@
-import strong, strong_a_o, strong_a_o_overloaded, strong_e_i, strong_double_mid_radical, strong_double_mid_radical_initial_cluster
+import strong, strong_a_o, strong_a_o_overloaded, strong_e_i
+import strong_double_mid_radical, strong_double_mid_radical_initial_cluster
+
 import defective, defective_a_a, defective_p3_only
+
 import quadriliteral
-import loan_romance, loan_english
-import irregular_ra, irregular_qal, irregular_gie, irregular_ta, irregular_ha, irregular_sar, irregular_jaf, irregular_ried
-import vaux_kien, vaux_kellu
+
 import weak
+
+import loan_romance, loan_english
+
+import irregular_ra, irregular_qal, irregular_gie, irregular_ta
+import irregular_ha, irregular_sar, irregular_jaf, irregular_ried
+import irregular_mar, irregular_kiel, irregular_wera
+
+import vaux_kien, vaux_kellu
+
